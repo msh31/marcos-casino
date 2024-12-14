90fff4fbe72071b55c6487c810b1d092e61c8920
Marco <mshendriks03@gmail.com>
2024-12-14 18:24:32 +0100

    asset + icons

--- sketch.js
+++ sketch.js
@@ -1,22 +1,25 @@
 // our image
-let hawk;
+let character, characterRotated, backgroundImage;
 
 // preload runs before everything else
-function preload()
-{
-	hawk = loadImage('/assets/hawk.png');
+function preload() {
+	character = loadImage("/assets/hawk.png");
+	characterRotated = loadImage("/assets/hawk_rotated.png");
+	backgroundImage = loadImage("/assets/game_bg.png");
 }
 
 // setup runs once at the start
-function setup()
-{
-	createCanvas(1280, 720);
+function setup() {
+	var canvas = createCanvas(1280, 720);
+	canvas.parent("game-window");
 }
 
 // draw runs every frame
-function draw()
-{
-	background(200, 200, 255);
+function draw() {
+	// background(200, 200, 255);
 
-	image(hawk, 200, 200);
+	//img, posX, poxY, width, height
+	image(backgroundImage, 0, 0, 1280, 720);
+	image(character, 200, 200, 100, 100);
+	image(characterRotated, 800, 200, 100, 100);
 }
